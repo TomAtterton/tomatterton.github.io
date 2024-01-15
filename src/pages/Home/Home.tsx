56ff9f90e94c25bdf65190dfe3e5987690c02b01
Tom Atterton <tpatterton@gmail.com>
2024-01-15 14:36:16 +0100

    Add Particle background

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,33 +1,44 @@
-import { Text, View } from "react-native";
-import { StatusBar } from "expo-status-bar";
+import {Pressable, Text, View} from "react-native";
+import {StatusBar} from "expo-status-bar";
 import styles from "./home.style";
 import SocialButton from "../../components/SocialButton";
-import { socialData } from "../../config/data";
+import {socialData} from "../../config/data";
+import ParticalBackground from "../../components/ParticalBackground";
+
 
 const Home = () => {
-  return (
-    <View style={styles.container}>
-      <View style={styles.contentContainer}>
-        <Text style={styles.title}>Tom Atterton</Text>
-        <Text style={styles.description}>
-          {
-            "\nPassionate Software Developer currently focusing on mobile development specializing in React Native.\n\n Check out my socials below!"
-          }
-        </Text>
-      </View>
-      <StatusBar style="auto" />
-      <View style={styles.listContainer}>
-        {socialData.map(({ type, url }) => (
-          <SocialButton type={type} url={url} />
-        ))}
-      </View>
-      <View style={styles.footerContainer}>
-        <Text style={styles.footerText}>
-          {"© 2024 Tom Atterton - All rights reserved. Hosted on GitHub."}
-        </Text>
-      </View>
-    </View>
-  );
+
+
+    return (<View style={styles.container}>
+            <ParticalBackground/>
+            <View style={styles.headerContainer}>
+                <Pressable
+                    onPress={() => {
+                    }}>
+                    <Text style={styles.headerText}>Projects</Text>
+                </Pressable>
+            </View>
+            <View style={styles.contentContainer}>
+                <Text style={styles.title}>Tom Atterton</Text>
+                <Text style={styles.description}>
+                    {
+                        "\nPassionate Software Developer currently focusing on mobile development specializing in React Native.\n\n Check out my socials below!"
+                    }
+                </Text>
+            </View>
+            <StatusBar style="auto"/>
+            <View style={styles.listContainer}>
+                {socialData.map(({type, url}) => (
+                    <SocialButton type={type} url={url}/>
+                ))}
+            </View>
+            <View style={styles.footerContainer}>
+                <Text style={styles.footerText}>
+                    {"© 2024 Tom Atterton - All rights reserved. Hosted on GitHub."}
+                </Text>
+            </View>
+        </View>
+    );
 };
 
 export default Home;
--- src/pages/Home/home.style.ts
+++ src/pages/Home/home.style.ts
@@ -1,36 +1,52 @@
-import { StyleSheet } from "react-native";
+import {StyleSheet} from "react-native";
 
 export default StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#4E6471",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  contentContainer: {
-    paddingHorizontal: 20,
-    paddingVertical: 20,
-    borderRadius: 20,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  title: {
-    color: "white",
-    fontSize: 60,
-    fontFamily: "roboto-bold",
-  },
-  description: {
-    textAlign: "center",
-    fontSize: 16,
-    fontFamily: "roboto-regular",
-    color: "#FFFFFF88",
-  },
-  listContainer: {
-    flexDirection: "row",
-  },
-  footerContainer: { position: "absolute", bottom: 16, left: 16, right: 0 },
-  footerText: {
-    fontFamily: "roboto-regular",
-    color: "#FFFFFF88",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#4E6471",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    headerContainer: {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        paddingTop: 16,
+        paddingHorizontal: 16,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
+    headerText: {
+        fontFamily: "roboto-bold",
+        color: "#FFFFFF",
+        fontSize: 24,
+    },
+    contentContainer: {
+        paddingHorizontal: 20,
+        paddingVertical: 20,
+        borderRadius: 20,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    title: {
+        color: "white",
+        fontSize: 60,
+        fontFamily: "roboto-bold",
+    },
+    description: {
+        textAlign: "center",
+        fontSize: 16,
+        fontFamily: "roboto-regular",
+        color: "#FFFFFF88",
+    },
+    listContainer: {
+        flexDirection: "row",
+    },
+    footerContainer: {position: "absolute", bottom: 16, left: 16, right: 0},
+    footerText: {
+        fontFamily: "roboto-regular",
+        color: "#FFFFFF88",
+    },
 });
